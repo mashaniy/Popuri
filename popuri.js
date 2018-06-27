a50896d93c10c2c97b4847c85b5a63e8d0c0eb3e
mashaniy <40450254+mashaniy@users.noreply.github.com>
2018-06-27 22:20:38 +0700

    Update popuri.js

--- popuri.js
+++ popuri.js
@@ -4,7 +4,7 @@ const config = require("./config.json"); // kita akan menaruh prefix dan token d
 
 bot.on("ready", async () => {
 	console.log(`${bot.user.username} Sudah online!`);
-	bot.user.setActivity("Youtube!", {type: "PLAYING"});	
+	bot.user.setActivity("Hero Of Leaf Valley", {type: "PLAY HM-"});	
 });
 
 bot.on("message", async message => {
@@ -18,6 +18,20 @@ bot.on("message", async message => {
 
     if (cmd === `${prefix}Popuri`) { 
 	      message.channel.send("halo!");
+    }
+	
+    if (cmd === `${prefix}Popuriinfo) {
+	let bicon = bot.user.displayAvatarURL; // untuk menampilkan avatar dari bot kalian
+	let botembed = new Discord.RichEmbed()
+		.setAuthor("Informasi Bot")
+		.setColor("RANDOM") // kalian juga bisa menggunakan kode HEX, cari di google
+		.setThumbnail(bicon) // thumbnail dari avatar bot kalian tadi
+		.addField("Nama Bot", bot.user.username)
+		.addField("Dibuat", bot.user.createdAt);
+		.setFooter("©haniy)
+        	.setTimestamp()
+
+		message.channel.send(botembed); // untuk mengirim embed yang sudah dibuat diatas..
     }
 });
 
